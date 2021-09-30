d15f1af85e6ae1ab54313802ba9ae0b95d8e3fa7
Josep Prat <josep.prat@aiven.io>
2021-09-30 19:58:25 +0200

    Add workaround

--- src/main/scala/ImpossibleCast.scala
+++ src/main/scala/ImpossibleCast.scala
@@ -1,7 +1,10 @@
 object ImpossibleCast {
   
+    val a = new TypedVarargs[java.lang.Long]()
+    val b = z.varArgs(1L) // this works
+
     val x = new TypedVarargs[Long]()
-    val y = x.varArgs(1L)
+    val y = x.varArgs(1L) // this throws
 
     def main(args: Array[String]): Unit = {
         println("runs")
